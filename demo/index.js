33470d7549582e350317a077dae1871e89a0ed81
dimamalyshev <199">
2020-04-14 17:09:16 +0300

    Setup npm publishing

---
+++ demo/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MTable from '../src';
+
+const target = document.getElementById('root');
+
+const Demo = () => (
+  <MTable />
+);
+
+ReactDOM.render(<Demo />, target);
---
+++ src/componets/MTableContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as PropTypes from 'prop-types';
+
+const MTableContainer = () => {
+
+  return (
+    <div>
+      Material UI Table
+    </div>
+  );
+};
+
+MTableContainer.propTypes = {};
+
+export default MTableContainer;
