731364a99cba76039c3fde4ca552801756d7377b
naum_d <olegovich1996@gmail.com>
2021-02-13 14:42:29 +0300

    Add row prop

--- src/components/MUIRow/MUIRow.jsx
+++ src/components/MUIRow/MUIRow.jsx
@@ -124,7 +124,7 @@ const MUIRow = ({ row, level, closed }) => {
         {!showForm && columns.map(({ field, cellProps, lookup }) => (
           <TableCell key={field} className={classes.cell} {...cellProps} >
             {!lookup && getFromObject(field, row)}
-            {!!lookup && lookup(getFromObject(field, row))}
+            {!!lookup && lookup(getFromObject(field, row), row)}
           </TableCell>
         ))}
 
